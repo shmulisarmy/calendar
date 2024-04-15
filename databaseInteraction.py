c377c9c15a42e885f93e196f4fd7f58074895c5c
shmuli <shmulikeller@gmail.com>
2024-04-14 23:52:37 -0400

    initial commit

---
+++ databaseInteraction.py
@@ -0,0 +1,53 @@
+from sqlite3 import connect
+
+
+
+def create_user_if_not_exists(name, password) -> bool|int:
+    conn = connect("database.db")
+    """Create a new user in the database."""
+    cursor = conn.execute("SELECT id FROM users WHERE name = ?", (name,))
+    if cursor.fetchone():
+        return False
+    conn.execute("INSERT INTO users (name, password) VALUES (?, ?)", (name, password))
+    cursor = conn.execute("SELECT id FROM users WHERE name = ?", (name,))
+    userId = cursor.fetchone()[0]
+    conn.commit()
+    return userId
+
+
+def returns_name_and_id_if_user_exists_and_password_is_correct(name, password) -> tuple["userId", "username"]|None:
+    conn = connect("database.db")
+    """to be used when user is logging in and in order to check if password is correct and if so get id"""
+    cursor = conn.execute("SELECT id, name FROM users WHERE name = ? AND password = ?", (name, password))
+    row = cursor.fetchone()
+    if row:
+        return row
+    return None
+
+def create_event(name: str, description: str, date: str, user_id) -> None:
+    conn = connect("database.db")
+    """Create a new event in the database."""
+    conn.execute("INSERT INTO events (name, description, date, user_id) VALUES (?, ?, ?, ?)", (name, description, date, user_id))
+    conn.commit()
+
+def get_events_by_userId(userId):
+    conn = connect("database.db")
+    """Retrieve events from the database using a username."""
+    # First, get the user_id for the given username
+    cursor = conn.execute("SELECT name, description, date FROM events WHERE user_id = ?", (userId,))
+    events = cursor.fetchall()
+
+    return events
+
+
+def display_events():
+    conn = connect("database.db")
+    """Retrieve events from the database."""
+    cursor = conn.execute("SELECT name, description, date, user_id FROM events")
+    events = cursor.fetchall()
+    return events
+
+
+
+print(f"{get_events_by_userId(1) = }")
+# print(*display_events(), sep='\n')
---
+++ main.py
@@ -0,0 +1,106 @@
+from flask import Flask, request, session, redirect, render_template
+from databaseInteraction import *
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+@app.route('/allEvents')
+def index():
+    userId = session.get('userId')
+    if not userId:
+        return redirect('/login')
+    events = get_events_by_userId(userId)
+    if not events:
+        return "you have no events"
+    calendar = {}
+    for event in events:
+        if event[2] not in calendar:
+            calendar[event[2]] = []
+        calendar[event[2]].append(event)
+    return render_template("index.html", calendar=calendar, name=session.get('username'))
+
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    name = request.form['name']
+    password = request.form['password']
+    rv = returns_name_and_id_if_user_exists_and_password_is_correct(name, password)
+    if rv is None:
+        return send_message("user not found or password incorrect")
+    print(f"{rv = }")
+    session['userId'] = rv[0] 
+    session['username'] = rv[1] 
+    return send_message("user logged in successfully")
+
+
+@app.route('/createEvent', methods=['POST', 'GET'])
+def createEvent():
+    if request.method == 'GET':
+        return render_template("createEvent.html")
+    userId = session.get('userId')
+    print(f"createEvent: {userId}")
+    if userId == None:
+        return send_message("you are not logged in")
+    name = request.form['name']
+    description = request.form['description']
+    date = request.form['date']
+    create_event(name, description, date, userId)
+    return send_message(f"event created successfully ({name = }{description = }{date = }{userId})")
+
+
+@app.route('/getEvents', methods=['GET'])  
+def getEvents():
+    userId = session.get('userId')
+    if userId == None:
+        return redirect('/login')
+    events = get_events_by_userId(userId)
+
+    if events is None:
+        return send_message("you have no events")
+    
+    days = {}
+    for event in events:
+        if event[2] not in days:
+            days[event[2]] = []
+        days[event[2]].append(event)
+
+    return events
+
+
+@app.route('/logout', methods=['POST', 'GET'])
+def logout():
+    session.clear()
+    
+
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'GET':
+        return render_template("signup.html")
+    
+    name = request.form['name']
+    password = request.form['password']
+
+    userId = create_user_if_not_exists(name, password)
+    if not userId:
+        return send_message("user already exists")
+    session['userId'] = userId
+    session['username'] = name
+    return send_message("user create successfully" )
+
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    return render_template("index.html", calendar=display_events())
+
+def send_message(message):
+    return render_template("base.html", message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
